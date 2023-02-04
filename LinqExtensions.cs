f17c4b6dca6019d7d19fa8b4457db20f646f85ed
NetDefender <danielsith@hotmail.com>
2023-02-04 21:56:27 +0100

    Add project files.

---
+++ LinqExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace SimdIteration
+{
+    internal static class LinqExtensions
+    {
+        #region methods
+        public static T[] Mutate<T>(this T[] instance, Func<int, T, T> action)
+            where T : struct, INumber<T>
+        {
+            ref T item = ref MemoryMarshal.GetReference(new Span<T>(instance));
+            ref T last = ref Unsafe.Add(ref item, instance.Length);
+            int pos = 0;
+
+            while (Unsafe.IsAddressLessThan(ref item, ref last))
+            {
+                item = action(pos++, item);
+                item = ref Unsafe.Add(ref item, 1);
+            }
+
+            return instance;
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+using SimdIteration;
+
+Random random = new(Environment.TickCount);
+int[] items = new int[10];
+items.Mutate((index, value) => random.Next(index, 1000) + value);
+Console.WriteLine(items.Length);
