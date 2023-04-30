02476e1fdfe991a88ff4a9442703b3b9bcf7a84f
NetDefender <danielsith@hotmail.com>
2023-04-30 10:25:28 +0200

    fix: simplify IntConverter.tt

--- IntConverter.cs
+++ IntConverter.cs
@@ -1,46 +1,16 @@
 ﻿using System.Runtime.CompilerServices;
 namespace SimdIteration;
 
+
 public sealed partial class IntConverter
 {
-	[MethodImpl(MethodImplOptions.Unmanaged)]
-	public bool Unmanaged(int value)
-	{
-		return (value & ~value) == 0;
-	}
-	[MethodImpl(MethodImplOptions.NoInlining)]
-	public bool NoInlining(int value)
-	{
-		return (value & ~value) == 0;
-	}
-	[MethodImpl(MethodImplOptions.ForwardRef)]
-	public bool ForwardRef(int value)
-	{
-		return (value & ~value) == 0;
-	}
-	[MethodImpl(MethodImplOptions.Synchronized)]
-	public bool Synchronized(int value)
-	{
-		return (value & ~value) == 0;
-	}
-	[MethodImpl(MethodImplOptions.NoOptimization)]
-	public bool NoOptimization(int value)
-	{
-		return (value & ~value) == 0;
-	}
-	[MethodImpl(MethodImplOptions.PreserveSig)]
-	public bool PreserveSig(int value)
-	{
-		return (value & ~value) == 0;
-	}
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public bool AggressiveInlining(int value)
-	{
-		return (value & ~value) == 0;
-	}
-	[MethodImpl(MethodImplOptions.InternalCall)]
-	public bool InternalCall(int value)
-	{
-		return (value & ~value) == 0;
-	}
+	public const string UNMANAGED = "Unmanaged";
+	public const string NOINLINING = "NoInlining";
+	public const string FORWARDREF = "ForwardRef";
+	public const string SYNCHRONIZED = "Synchronized";
+	public const string NOOPTIMIZATION = "NoOptimization";
+	public const string PRESERVESIG = "PreserveSig";
+	public const string AGGRESSIVEINLINING = "AggressiveInlining";
+	public const string SECURITYMITIGATIONS = "SecurityMitigations";
+	public const string INTERNALCALL = "InternalCall";
 }
